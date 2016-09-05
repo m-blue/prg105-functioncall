30f6287d0a9a2dc7968e5b029bd3e604cdd563bf
m-blue <m-blue@users.noreply.github.com>
2016-09-05 14:28:27 -0500

    Add files via upload

---
+++ Function.py
@@ -0,0 +1,8 @@
+def print_order():
+    print("One cheeseburger, no onions")
+    print("Large fries, lightly salted")
+    print("Medium soda, no ice")
+
+print_order()
+print_order()
+print_order()
